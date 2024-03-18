24bbf194dc18b4df22d63c390dfcb94dc2e5b10b
koeninger.a <koeninger.a@northeastern.edu>
2024-03-18 16:23:06 -0400

    in class 3/18

--- models/book.js
+++ models/book.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema(
-  {}
+  {
+    title: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    summary: {type: String, required: true},
+    IBSN: {type: String},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+  }
 );
 
 //Export model
--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,12 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
+  BookInstance.find({"status" : {$eq:"Available"}})
+    .populate('book')
+    .exec()
+    .then(list_bookinstances => {
+      res.send(list_bookinstances.map(function(bookinstance) {
+        return bookinstance.book.title + " : " + bookinstance.status;
+      }))
+    })
 }
